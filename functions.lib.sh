53a4db017df588811f8f329f09b401c538313237
netzturbine <w0rk3r@netzturbines-MacBook-Air-2.local>
2012-09-07 05:26:04 +0200

    Revert "Revert "Revert "cleaned code"""
    This reverts commit e1ac96495d6cb4d901d739419d570567393806a6.

--- functions.lib.sh
+++ functions.lib.sh
@@ -1,15 +1,14 @@
 #! /bin/bash
-####################################################################################################
-# SRC: https://github.com/netzturbine/android-build-helper-shell-scripts.git                       #
-# AUTHOR: arnd (at) netzturbine (dot) de                                                           #
-# VERSION: 0.1b                                                                                    #
-# DESCRIPTION:                                                                                     #
-# library which contains some shell functions/settings used in this script bundle                  #
-# sourced by config.inc.sh by default                                                              #
-# all funtions starting w/ F__ are defined in this file                                            #
-#                                                                                                  #
-# for further Information see README.txt/README.md                                                 #
-####################################################################################################
+#############################################################################
+# file: functions.lib.sh
+# author: arnd (at) netzturbine (dot) de
+# version 0.1b
+# DESCRIPTION: 
+# library which contains some shell functions/settings used in this script bundle
+# sourced by config.inc.sh by default
+# 
+# further Irmation see README.txt/README.md
+##############################################################################
 
 # define colours
 export BLACK="\033[0;30m"
@@ -31,34 +30,11 @@ export WHITE="\033[1;37m"
 export NC="\033[1;0m"              # No Color
 
 
-function F__getBasedir()
+function getBasedir()
 {
-	BASEDIR=$(cd $(dirname "$0"); pwd);
-	
-	case "${BASEDIR}" in 
-        [./]*)
-            local ABSPATH="$(cd ${1%/*}; pwd)/${1##*/}"
-            echo "${ABSPATH/\/\///}"
-        ;;
-        *)
-            echo "${PWD}/${BASEDIR}";
-        ;;
-    esac
-    
-    export BASEDIR=${BASEDIR};
-
-}
-
-function F__resetVars(){
-	PREFIX=${1};
-	F__log "unsetting ${PREFIX} vars" "INF";
-	PARAMS=`env | grep ${PREFIX}`;
-	for PARAM in ${PARAMS}
-	do
-	    UNSET=$(echo ${PARAM} | awk -F= '{print $1}' );
-	    unset ${UNSET}
-    	#F__log "unset ${UNSET}" "INF";
-	done
+	BASEDIR=$(dirname $0);
+	#echo ${BASEDIR};
+	export BASEDIR=${BASEDIR};
 }
 
 ###################################################################################################
@@ -67,8 +43,7 @@ function F__resetVars(){
 # throws a warning if no OS is configured in config.inc.sh and checks against "Linux" and "Darwin" 
 # throws an error and exits if no MANDATORY OS is found
 ###################################################################################################
-
-function F__checkOS()
+function checkOS()
 {
      CHECKPLATFORMS="${1}";
      if [[ ${CHECKPLATFORMS} == "osx" ]]
@@ -79,9 +54,9 @@ function F__checkOS()
      if [[ -z ${CHECKPLATFORMS}  ]]
      then
          CHECKPLATFORMS="Linux Darwin";
-         F__log "PLATFORM not set in conf using: ${CHECKPLATFORMS}" "WARN";
+         log "PLATFORM not set in conf using: ${CHECKPLATFORMS}" "WARN";
      else
-     	F__log "${CHECKPLATFORMS} PLATFORM set in conf" "INF";
+     	log "${CHECKPLATFORMS} PLATFORM set in conf" "INF";
      fi
      
 	 UNAMESTRING=`uname`;
@@ -98,10 +73,10 @@ function F__checkOS()
     done
     if [[ -z ${FOUND} ]]
     then
-        F__log "PLATFORM ${UNAMESTRING} not recognized ... bailing out" "ERR";        
+        log "PLATFORM ${UNAMESTRING} not recognized ... bailing out" "ERR";        
         echo -e "${RED}ERR:${NC} PLATFORM ${UNAMESTRING} not recognized ... bailing out"
     else
-         F__log "found supported ${OS} PLATFORM" "INF";
+         log "found supported ${OS} PLATFORM" "INF";
          export PLATFORM=${OS};     
     fi
             
@@ -109,15 +84,13 @@ function F__checkOS()
 	unset UNAMESTRING;
 	unset CHECKPLATFORMS;
 }
-
 ###################################################################################################
 # checks for neccessary root privileges
 # CALLED: checkRoot "true" or "false" 
 # throws a warning that root privileges would be better if called w/ "false"
 # throws an error and exits if root privileges are MANDATORY if called w/ "true" or no param
 ###################################################################################################
-
-function F__checkRoot()
+function checkRoot()
 {
    
    MUSTHAVE=${1};   
@@ -125,47 +98,42 @@ function F__checkRoot()
    then
       if [[ MUSTHAVE == "false" ]]
       then
-    	F__log "dear ${F__logNAME} you should be root to use this script ... continue" "WARN";
+    	log "dear ${LOGNAME} you should be root to use this script ... continue" "WARN";
       else
-      	F__log "dear ${F__logNAME} you MUST be root to use this script properly... bailing out" "ERR";
+      	log "dear ${LOGNAME} you MUST be root to use this script properly... bailing out" "ERR";
       	exit 1;
       fi
   else
-      F__log "script called w/ neccessary root privileges ... continueing" "INF";
+      log "script called w/ neccessary root privileges ... continueing" "INF";
    fi
 }
-
 ###################################################################################################
 # checks if not root
 # CALLED: checkNonRoot "true" or "false" 
 # throws a warning that non root privileges would be better if called w/ "false"
 # throws an error and exits if root privileges are PROHIBITED if called w/ "true" or no param
 ###################################################################################################
-
-function F__checkNonRoot()
+function checkNonRoot()
 {
    MUSTNOTHAVE=${1};
    if [[ ${LOGNAME} == "root" ]]
    then
       if [[ ${MUSTNOTHAVE} == "false" ]]
       then
-        F__log "dear ${F__logNAME} you should NOT be root to use this script ... bailing out" "WARN";
+        log "dear ${LOGNAME} you should NOT be root to use this script ... bailing out" "WARN";
       else
-      	F__log "dear ${F__logNAME} NEVER be root to use this script ... bailing out" "ERR";
+      	log "dear ${LOGNAME} NEVER be root to use this script ... bailing out" "ERR";
       exit 1;
       fi
   else
-      F__log "script called w/o root privileges continuing" "INF";
+      log "script called w/o root privileges continuing" "INF";
    fi
 }
-
-###################################################################################################
 # checks if a given dir is writeable for a ${FACILITY}
 # if not tries to create it if ${CREATE} flag is set to true
 # bails out if everything fails
-###################################################################################################
 
-function F__checkForDir() 
+function checkForDir() 
 {
     FACILITY=${1};
 	CHECKDIR=${2};
@@ -175,9 +143,9 @@ function F__checkForDir()
 	then 
 	    if [ -w ${CHECKDIR} ]
     	then
-    	    F__log "found writable ${CHECKDIR} to enable ${FACILITY}" "INF";
+    	    log "found writable ${CHECKDIR} to enable ${FACILITY}" "INF";
     	else
-	    	F__log "${CHECKDIR} needed by ${FACILITY} not writable by ${USER} ...bailing out" "ERR";
+	    	log "${CHECKDIR} needed by ${FACILITY} not writable by ${USER} ...bailing out" "ERR";
    			echo -e "${RED}ERR:${NC} ${CHECKDIR} needed by ${FACILITY} not writable by ${USER} ...bailing out";
    			exit 1;
         fi   
@@ -186,9 +154,9 @@ function F__checkForDir()
     then
         if mkdir -p ${CHECKDIR};
         then
-    		F__log "created ${CHECKDIR} needed by ${FACILITY}" "WARN";
+    		log "created ${CHECKDIR} needed by ${FACILITY}" "WARN";
     	else
-    	    F__log  "could not create dir: ${CHECKDIR} needed by ${FACILITY} ...bailing out" "ERR";
+    	    log  "could not create dir: ${CHECKDIR} needed by ${FACILITY} ...bailing out" "ERR";
     	    echo "${RED}ERR:${NC} could not create dir: ${CHECKDIR} needed by ${FACILITY} ...bailing out";
     		exit 1;
     	fi
@@ -198,11 +166,11 @@ function F__checkForDir()
 	unset CREATE;
 }
 
-# F__logs w/ timestamp a given message to a given F__logFACILITY
-# tests via function F__"checkForDir" the F__logdir
+# logs w/ timestamp a given message to a given LOGFACILITY
+# tests via function "checkForDir" the logdir
 #
 
-function F__log() 
+function log() 
 {
 	MSG="${1}";
 	LOGLEVEL="${2}";
